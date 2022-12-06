00414db931b501a694cac5a0d2a4804ed7a96ecf
Nithin1306 <allanithin13@gmail.com>
2022-12-06 20:00:43 +0530

    initial commit

---
+++ Front-End/src/App.js
@@ -0,0 +1,13 @@
+import { ImageUpload } from "./home.js";
+import { SolUpload }  from "./next.js";
+import { Router } from "react-router-dom";
+
+function App() {
+  return(
+  <ImageUpload />
+  
+  
+  );
+}
+
+export default App;
